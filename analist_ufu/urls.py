5cd452d2ade85fa4b3636eda9b3b28c1d7c101bc
Margarido Sebastiao Vilarinho <msebastv@everis.com>
2020-07-14 01:44:49 -0300

    criação de login e logout

--- analist_ufu/urls.py
+++ analist_ufu/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import  static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls')),
     path('', include('principal.urls')),
-    path('accounts/',include('django.contrib.auth.urls'))
 ]
 
 if settings.DEBUG:
