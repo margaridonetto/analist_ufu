4e71801fcfec1c263472a35cb2f48f668ec3f283
Margarido Sebastiao Vilarinho <msebastv@everis.com>
2020-07-13 21:42:05 -0300

    Inclusão Editar e Apagar

--- principal/urls.py
+++ principal/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    path('new/', views.PostCreate.as_view(), name='new'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='detail'),
+    path('<slug:slug>/edit/', views.PostUpdate.as_view(), name='edit'),
+    path('<slug:slug>/delete/', views.PostDelete.as_view(), name='delete'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 from . models import Post
 # Create your views here.
 
@@ -11,3 +13,19 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'principal/detail.html'
     context_object_name = 'customizado'
+
+class PostCreate(CreateView):
+    model = Post
+    template_name = 'principal/newpost.html'
+    fields = ('autor','titulo','conteudo')
+
+class PostUpdate(UpdateView):
+    model = Post
+    template_name = 'principal/edit.html'
+    fields = ('titulo','conteudo')
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'principal/delete.html'
+    success_url = reverse_lazy("home")
+
